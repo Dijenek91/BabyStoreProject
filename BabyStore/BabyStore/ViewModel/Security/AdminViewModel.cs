c6c6d762395fede75c333e7bc0da4618cf99ea09
Dijenek91 <39437768+Dijenek91@users.noreply.github.com>
2020-04-02 11:49:25 +0200

    Security roles handling and user model (before 2 context migrations)

---
+++ BabyStore/BabyStore/ViewModel/Security/AdminViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BabyStore.ViewModel.Security
+{
+    public class RoleViewModel
+    {
+        public string Id { get; set; }
+        [Required(AllowEmptyStrings = false)]
+        [Display(Name = "Role Name")]
+        public string Name { get; set; }
+    }
+}
