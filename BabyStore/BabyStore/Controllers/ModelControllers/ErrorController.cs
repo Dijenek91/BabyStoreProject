3ee5159bf83d7bc40dc7a8fa6aa9f8fe0bc983fe
Dijenek91 <39437768+Dijenek91@users.noreply.github.com>
2020-04-10 16:02:21 +0200

    Custom error views added

--- BabyStore/BabyStore/Controllers/ModelControllers/ErrorController.cs
+++ BabyStore/BabyStore/Controllers/ModelControllers/ErrorController.cs
@@ -8,6 +8,18 @@ namespace BabyStore.Controllers.ModelControllers
 {
     public class ErrorController : Controller
     {
+        public ActionResult PageNotFound()
+        {
+            Response.StatusCode = 404;
+            return View();
+        }
+
+        public ActionResult InternalServerError()
+        {
+            Response.StatusCode = 500;
+            return View();
+        }
+
         // GET: Error
         public ActionResult FileUploadLimitExceeded()
         {
