70fbca9111f5db3610ab06965aae10f017ab7aa5
Dijenek91 <39437768+Dijenek91@users.noreply.github.com>
2020-04-23 19:40:22 +0200

    ViewFix

--- BabyStore/BabyStore/App_Start/BundleConfig.cs
+++ BabyStore/BabyStore/App_Start/BundleConfig.cs
@@ -25,6 +25,10 @@ namespace BabyStore
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //         "~/Content/store.css",
+            //         "~/Content/Reset/reset.css"));
         }
     }
 }
